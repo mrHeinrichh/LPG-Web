a42419c6bcd2d1666406b6285330368070293570
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-30 19:53:59 +0800

    Update Navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,26 +1,10 @@
 import React from "react";
 import style from "./style.module.css";
-import Image from "next/image";
-import logo from "@/public/logo.png";
-import { Button } from "@/components";
 
 function Navbar() {
   return (
     <>
-      <div className={style.container}>
-        <h3>LPG ADMIN</h3>
-
-        <div className={style.links_container}>
-          <p>Marketplace</p>
-          <p>Community</p>
-          <p>Events</p>
-        </div>
-
-        <div className={style.links_container}>
-          <Button outlined>Sign In</Button>
-          <Button>Get Started</Button>
-        </div>
-      </div>
+      <div className={style.container}></div>
       <div className={style.divider}></div>
     </>
   );
