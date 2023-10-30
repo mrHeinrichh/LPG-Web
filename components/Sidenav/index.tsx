3d162822ba9f291ade8a4aad0bba5417eafaf37f
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-30 20:21:29 +0800

    Update Sidenav

--- components/Sidenav/index.tsx
+++ components/Sidenav/index.tsx
@@ -4,9 +4,48 @@ import style from "./style.module.css";
 import { Button, Navbar } from "@/components";
 
 function Sidenav({ children }: any) {
+  const links: any[] = [
+    {
+      title: "Dashboard",
+    },
+    {
+      title: "Transactions",
+    },
+    {
+      title: "Feedbacks",
+    },
+    {
+      title: "Chats",
+    },
+    {
+      title: "Order History",
+    },
+    {
+      title: "Appointment",
+    },
+    {
+      title: "Products",
+    },
+    {
+      title: "Customers",
+    },
+    {
+      title: "Retailers",
+    },
+    {
+      title: "Riders",
+    },
+  ];
   return (
     <>
-      <div className={style.sidenav}></div>
+      <div className={style.sidenav}>
+        <div className={style.header}></div>
+        {links.map((e: any) => (
+          <div key={e.title} className={style.link_container}>
+            <p>{e.title} </p>
+          </div>
+        ))}
+      </div>
       <div className={style.container}>
         <div className={style.divider}></div>
         <div>
