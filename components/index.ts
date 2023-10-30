67e5997dbc63c2cfa7440e0784a9e91996cbbf0e
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-31 00:03:45 +0800

    Barrel components

--- components/index.ts
+++ components/index.ts
@@ -4,4 +4,5 @@ import Sidenav from "./Sidenav";
 import Card from "./Card";
 import StatsCard from "./StatsCard";
 import Datatable from "./Datatable";
-export { Datatable, StatsCard, Card, Sidenav, Navbar, Button };
+import TableRow from "./TableRow";
+export { TableRow, Datatable, StatsCard, Card, Sidenav, Navbar, Button };
