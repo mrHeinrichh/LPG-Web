62bb4f463ec597cf059284ccc7d53065b7959c47
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-31 15:26:36 +0800

    Update users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,22 @@
 import { TableRow, Datatable, Sidenav } from "@/components";
+import trash from "@/public/trash.svg";
+import Image from "next/image";
 
 export async function getUsers() {
   const response = await fetch(
     "https://lpg-api-06n8.onrender.com/api/v1/users"
+  );
+
+  const data = (await response.json()).data;
+  return data;
+}
+
+export async function deleteUsers(id: any) {
+  const response = await fetch(
+    `https://lpg-api-06n8.onrender.com/api/v1/users/${id}`,
+    {
+      method: "DELETE",
+    }
   );
 
   const data = (await response.json()).data;
@@ -24,7 +38,9 @@ export default async function Transactions({}: any) {
               <td>{e.name}</td>
               <td>{e.contactNumber}</td>
               <td>{e.__t}</td>
-              <td></td>
+              <td>
+                <Image src={trash} alt={"trash"}></Image>
+              </td>
             </TableRow>
           ))}
         </Datatable>
