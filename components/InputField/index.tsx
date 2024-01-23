83e389d0fd2787fe27063d81fa28b2c63db3be43
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-24 06:14:53 +0800

    Add components

--- components/InputField/index.tsx
+++ components/InputField/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import style from "./style.module.css";
 
-function InputField({ onChange, name, type, placeholder, defaultValue }: any) {
+interface IInputField {
+  onChange?: (event: any) => void;
+  name?: string;
+  type?: string;
+  placeholder?: string;
+  defaultValue?: string;
+}
+
+function InputField({
+  onChange,
+  name,
+  type,
+  placeholder,
+  defaultValue,
+}: IInputField) {
   return (
-    <>
+    <div className="flex flex-col gap-2">
+      {placeholder ? <p>{placeholder}</p> : <></>}
       <input
         className={style.input}
         type={type ?? "text"}
@@ -12,7 +27,7 @@ function InputField({ onChange, name, type, placeholder, defaultValue }: any) {
         placeholder={placeholder ?? "placeholder"}
         onChange={onChange ?? (() => {})}
       ></input>
-    </>
+    </div>
   );
 }
 
--- components/index.ts
+++ components/index.ts
@@ -6,8 +6,10 @@ import StatsCard from "./StatsCard";
 import Datatable from "./Datatable";
 import TableRow from "./TableRow";
 import InputField from "./InputField";
+import SelectField from "./SelectField";
 
 export {
+  SelectField,
   InputField,
   TableRow,
   Datatable,
