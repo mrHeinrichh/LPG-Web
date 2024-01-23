9881fae3e8216836b213cfb903c8899443ddeb17
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-24 06:16:07 +0800

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
