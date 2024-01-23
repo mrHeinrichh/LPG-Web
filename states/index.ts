772da69770237e7cbee4095b3a126501d668b658
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-24 06:15:34 +0800

    Add states

--- states/index.ts
+++ states/index.ts
@@ -1,26 +1,43 @@
+import { get, remove } from "@/config";
 import { API_URL } from "@/env";
 import { create } from "zustand";
 
+export async function deleteCustomer(id: string) {
+  const response = await fetch(`${API_URL}users/${id}`, { method: "DELETE" });
+  const data = (await response.json()).data;
+  console.log(data);
+  return data;
+}
+
 export const useRiderStore = create((set) => ({
   riders: [],
-  getRider: (data: any) => set((state: any) => ({ riders: data })),
+  getRider: async () => {
+    const { data } = await get(`users?filter={"__t": "Rider"}`);
+    return set(() => ({ riders: data.data }));
+  },
+  removeRider: async (id: string) => {
+    const { data } = await remove(`users/${id}`);
+    if (data.state == "success") {
+      return set((state: any) => ({
+        items: [...state.riders.filter((e: any) => e._id != id)],
+      }));
+    }
+  },
 }));
 
 export const useCustomerStore = create((set) => ({
   customers: [],
   getCustomer: async () => {
-    const response = await fetch(`${API_URL}users?filter={"__t": "Customer"}`);
-    const data = (await response.json()).data;
-    return set(() => ({ customers: data }));
+    const { data } = await get(`users?filter={"__t": "Customer"}`);
+    return set(() => ({ customers: data.data }));
   },
 }));
 
 export const useItemStore = create((set) => ({
   items: [],
   getItems: async () => {
-    const response = await fetch(`${API_URL}items`);
-    const data = (await response.json()).data;
-    return set(() => ({ items: data }));
+    const { data } = await get(`items`);
+    return set(() => ({ items: data.data }));
   },
   removeItem: (id: any) => {
     set((state: any) => ({
