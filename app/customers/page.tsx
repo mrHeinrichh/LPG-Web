30757d5b72123b347746d0f3f45fcb88d9d8a56d
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-24 12:24:57 +0800

    Add customers edit

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { Button, Datatable, Sidenav } from "@/components";
+import { remove } from "@/config";
+import edit from "@/public/edit.svg";
 import trash from "@/public/trash.svg";
 import { useCustomerStore } from "@/states";
 import Image from "next/image";
@@ -9,13 +11,14 @@ import { useEffect } from "react";
 export default function Customers({}: any) {
   const router = useRouter();
 
-  const { getCustomer, customers } = useCustomerStore() as any;
+  const { getCustomer, customers, removeCustomer } = useCustomerStore() as any;
 
   const header: any[] = [
     "Name",
     "Contact Number",
+    "Email",
     "Address",
-    "Address",
+    "Verified",
     "Discounted",
     "Action",
   ];
@@ -28,7 +31,7 @@ export default function Customers({}: any) {
     <>
       <Sidenav>
         <div className="flex justify-between items-center w-full">
-          <h4>Items</h4>
+          <h4>Customers</h4>
           <Button
             onClick={() => {
               router.push("/customers/add");
@@ -43,11 +46,25 @@ export default function Customers({}: any) {
             <tr key={e._id}>
               <td>{e.name}</td>
               <td>{e.contactNumber}</td>
+              <td>{e.email}</td>
               <td>{e.address}</td>
               <td>{e.verified ? "Verfied" : "Pending"}</td>
               <td>{e.discounted ? "Discounted" : "Not Discounted"}</td>
-              <td>
-                <Image src={trash} alt={"trash"}></Image>
+              <td className="flex justify-evenly">
+                <div
+                  onClick={() => {
+                    router.push(`/customers/edit?id=${e._id}`);
+                  }}
+                >
+                  <Image src={edit} alt={"edit"}></Image>
+                </div>
+                <div
+                  onClick={() => {
+                    removeCustomer(e._id);
+                  }}
+                >
+                  <Image src={trash} alt={"trash"}></Image>
+                </div>
               </td>
             </tr>
           ))}
