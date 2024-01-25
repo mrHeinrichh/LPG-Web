9b34f91f9544585f1add2d10be08821660fa2ca4
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-25 12:38:07 +0800

    Update nextConfig

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
     ],
   },
 };
