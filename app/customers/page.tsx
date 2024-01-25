d031e5d2789e13cb70ff138ad7198bdc2d2f6e94
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-25 12:42:06 +0800

    Update customers

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import { Button, Datatable, Sidenav } from "@/components";
-import { remove } from "@/config";
 import edit from "@/public/edit.svg";
 import trash from "@/public/trash.svg";
 import { useCustomerStore } from "@/states";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 export default function Customers({}: any) {
   const router = useRouter();
 
-  const { getCustomer, customers, removeCustomer } = useCustomerStore() as any;
+  const { getCustomer, customers, removeCustomer, toggleVerify } =
+    useCustomerStore() as any;
 
   const header: any[] = [
     "Name",
@@ -23,9 +23,14 @@ export default function Customers({}: any) {
     "Action",
   ];
 
+  const unverified = useMemo(
+    () => customers.filter((e: any) => !e.verified),
+    [customers]
+  );
+
   useEffect(() => {
     getCustomer();
-  }, []);
+  }, [customers]);
 
   return (
     <>
@@ -41,6 +46,25 @@ export default function Customers({}: any) {
           </Button>
         </div>
 
+        <div className="flex flex-col gap-2 w-1/2">
+          <p>Pending</p>
+          {unverified.map((e: any) => {
+            return (
+              <div key={e._id} className=" bg-slate-800 p-3 rounded">
+                <p className="text-white-100">{e.name}</p>
+
+                <Button
+                  onClick={() => {
+                    toggleVerify(e._id, true);
+                  }}
+                >
+                  <p className="text-white-100">Approve</p>
+                </Button>
+              </div>
+            );
+          })}
+        </div>
+
         <Datatable header={header}>
           {customers.map((e: any) => (
             <tr key={e._id}>
@@ -48,7 +72,7 @@ export default function Customers({}: any) {
               <td>{e.contactNumber}</td>
               <td>{e.email}</td>
               <td>{e.address}</td>
-              <td>{e.verified ? "Verfied" : "Pending"}</td>
+              <td>{e.verified ? "Verified" : "Pending"}</td>
               <td>{e.discounted ? "Discounted" : "Not Discounted"}</td>
               <td className="flex justify-evenly">
                 <div
