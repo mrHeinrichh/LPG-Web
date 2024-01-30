2470c6d7c2799ce8b0fa5d27ba8b7c6eb5255679
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-30 09:41:35 +0800

    Update dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,45 @@
 "use client";
-import { StatsCard, Sidenav, Button } from "@/components";
+import { StatsCard, Sidenav, Button, InputField } from "@/components";
 import {
   useCustomerStore,
   useDashboardStore,
   useTransactionStore,
 } from "@/states";
-import { PureComponent, useEffect, useMemo } from "react";
+import { PureComponent, useEffect, useMemo, useState } from "react";
 import {
-  Bar,
-  BarChart,
-  ResponsiveContainer,
-  Area,
-  XAxis,
-  YAxis,
-  Tooltip,
-  CartesianGrid,
+  PieChart,
+  Pie,
   Legend,
+  Tooltip,
+  ResponsiveContainer,
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  Bar,
   Rectangle,
 } from "recharts";
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-function Example() {
-  return (
-    <BarChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar
-        dataKey="pv"
-        fill="#8884d8"
-        activeBar={<Rectangle fill="pink" stroke="blue" />}
-      />
-      <Bar
-        dataKey="uv"
-        fill="#82ca9d"
-        activeBar={<Rectangle fill="gold" stroke="purple" />}
-      />
-    </BarChart>
-  );
-}
 
+const data01 = [
+  { name: "Group A", value: 1000000 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+  { name: "Group E", value: 278 },
+  { name: "Group F", value: 189 },
+];
+
+const data02 = [
+  { name: "Group A", value: 2400 },
+  { name: "Group B", value: 4567 },
+  { name: "Group C", value: 1398 },
+  { name: "Group D", value: 9800 },
+  { name: "Group E", value: 3908 },
+  { name: "Group F", value: 4800 },
+];
 export default function Home() {
   const dashboardStore = useDashboardStore() as any;
   const { getCustomer, customers, toggleVerify } = useCustomerStore() as any;
-
+  const [startDate, setstartDate] = useState("01/22/2023");
   const unverified = useMemo(
     () => customers.filter((e: any) => !e.verified),
     [customers]
@@ -107,6 +49,98 @@ export default function Home() {
     () => dashboardStore.transactions.filter((e: any) => e.status == "Pending"),
     [dashboardStore.transactions]
   );
+  const deliveryTypeCounts = useMemo(() => {
+    return [
+      {
+        name: "Online",
+        num: dashboardStore.transactions.reduce((acc: any, curr: any) => {
+          return curr.__t == "Delivery" ? acc + 1 : acc;
+        }, 0),
+      },
+      {
+        name: "Walk In",
+        num: dashboardStore.transactions.reduce((acc: any, curr: any) => {
+          return curr.__t == null ? acc + 1 : acc;
+        }, 0),
+      },
+    ];
+  }, [dashboardStore.transactions]);
+
+  const itemTypeCounts = useMemo(() => {
+    const temp = dashboardStore.transactions.map(
+      (transaction: any) => transaction.items
+    );
+    const items = [].concat.apply([], temp);
+    return [
+      {
+        name: "Product",
+        value: items.reduce((acc: any, curr: any) => {
+          return curr.type == "Product" ? acc + 1 : acc;
+        }, 0),
+      },
+      {
+        name: "Accessories",
+        value: items.reduce((acc: any, curr: any) => {
+          return curr.type == "Accessories" ? acc + 1 : acc;
+        }, 0),
+      },
+    ];
+  }, [dashboardStore.transactions]);
+
+  const itemCounts = useMemo(() => {
+    const temp = dashboardStore.transactions.map(
+      (transaction: any) => transaction.items
+    );
+    const items = [].concat.apply([], temp);
+    const names = [...new Set(items.map((e: any) => e.name))];
+    return names.map((e: any) => ({
+      name: e,
+      value: items.reduce((acc: any, curr: any) => {
+        return curr.name == e ? curr.quantity + acc : acc;
+      }, 0),
+    }));
+  }, [dashboardStore.transactions]);
+
+  const deliveryCounts = useMemo(() => {
+    return [
+      {
+        name: "Pending",
+        value: dashboardStore.transactions.reduce((acc: any, curr: any) => {
+          return curr.status == "Pending" ? acc + 1 : acc;
+        }, 0),
+      },
+      {
+        name: "Approved",
+        value: dashboardStore.transactions.reduce((acc: any, curr: any) => {
+          return curr.status == "Approved" ? acc + 1 : acc;
+        }, 0),
+      },
+      {
+        name: "Declined",
+        value: dashboardStore.transactions.reduce((acc: any, curr: any) => {
+          return curr.status == "Declined" ? acc + 1 : acc;
+        }, 0),
+      },
+      {
+        name: "Completed",
+        value: dashboardStore.transactions.reduce((acc: any, curr: any) => {
+          return curr.status == "Completed" ? acc + 1 : acc;
+        }, 0),
+      },
+      {
+        name: "On Going",
+        value: dashboardStore.transactions.reduce((acc: any, curr: any) => {
+          return curr.status == "On Going" ? acc + 1 : acc;
+        }, 0),
+      },
+      {
+        name: "Cancelled",
+        value: dashboardStore.transactions.reduce((acc: any, curr: any) => {
+          return curr.status == "Cancelled" ? acc + 1 : acc;
+        }, 0),
+      },
+    ];
+  }, [dashboardStore.transactions]);
 
   const completedDeliveries = useMemo(
     () =>
@@ -115,8 +149,6 @@ export default function Home() {
   );
 
   const total = useMemo(() => {
-    console.log(completedDeliveries);
-
     return completedDeliveries.reduce(
       (acc: any, curr: any) => acc + curr.total,
       0
@@ -125,12 +157,22 @@ export default function Home() {
 
   useEffect(() => {
     getCustomer();
-    dashboardStore.getTransactionsByStatus(["Pending", "Completed"]);
+    // dashboardStore.getTransactionsByStatus(["Pending", "Completed"]);
+    dashboardStore.getTransactions();
   }, []);
 
+  const startDateChange = () => {};
   return (
     <main>
       <Sidenav>
+        <p>{startDate}</p>
+
+        <input type="date" id="start" defaultValue={"03/22/2023"} />
+        {/* <InputField
+          type="date"
+          onChange={startDateChange}
+          defaultValue={startDate}
+        /> */}
         <div className="grid grid-cols-3 gap-2 w-full">
           <StatsCard title="Total Revenue Today" value={total} net={-0.0} />
           <StatsCard
@@ -144,8 +186,83 @@ export default function Home() {
             net={1.7}
           />
         </div>
-        <div className="w-full">
-          <Example />
+        <div className="w-full flex items-center">
+          <div className="flex flex-col items-center">
+            <p>Walkins and Online</p>
+            <BarChart
+              width={400}
+              height={300}
+              data={deliveryTypeCounts}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <Tooltip />
+              <Legend />
+              <XAxis dataKey="name" />
+              <Bar
+                dataKey="num"
+                fill="#8884d8"
+                activeBar={<Rectangle fill="pink" stroke="blue" />}
+              />
+            </BarChart>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <p>Item Types</p>
+            <PieChart width={300} height={300}>
+              <Pie
+                dataKey="value"
+                isAnimationActive={false}
+                data={itemTypeCounts}
+                cx="50%"
+                cy="50%"
+                outerRadius={100}
+                fill="#8884d8"
+                label
+              />
+
+              <Tooltip />
+            </PieChart>
+          </div>
+          <div className="flex flex-col items-center">
+            <p>Products Sold</p>
+            <PieChart width={300} height={300}>
+              <Pie
+                dataKey="value"
+                isAnimationActive={false}
+                data={itemCounts}
+                cx="50%"
+                cy="50%"
+                outerRadius={100}
+                fill="#8884d8"
+                label
+              />
+
+              <Tooltip />
+            </PieChart>
+          </div>
+          <div className="flex flex-col items-center">
+            <p>Delivery Statuses</p>
+            <PieChart width={300} height={300}>
+              <Pie
+                dataKey="value"
+                isAnimationActive={false}
+                data={deliveryCounts}
+                cx="50%"
+                cy="50%"
+                outerRadius={100}
+                fill="#8884d8"
+                label
+              />
+
+              <Tooltip />
+            </PieChart>
+          </div>
         </div>
         <div className="grid grid-cols-3 gap-2 w-full">
           <div className="flex flex-col gap-2">
