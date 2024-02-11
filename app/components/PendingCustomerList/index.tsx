3aa9ff0bace21be33f95150e8a69f9c3d4677c04
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-02-12 07:41:22 +0800

    Update

--- app/components/PendingCustomerList/index.tsx
+++ app/components/PendingCustomerList/index.tsx
@@ -45,6 +45,30 @@ export default function PendingCustomerList() {
           </div>
         </div>
       }
-    ></ApprovalsList>
+    >
+      {pendings.map((e: any) => {
+        return (
+          <Card key={e._id}>
+            <div className="w-full flex items-center justify-between p-3">
+              <div className="flex items-center gap-2">
+                <Image src={e.image} alt="" height={100} width={100} />
+                <div className="flex flex-col">
+                  <p className="text-lg font-semibold">{e.name}</p>
+                  <p className="">{e.email}</p>
+                </div>
+              </div>
+
+              <Button
+                onClick={() => {
+                  verifyCustomer(e._id);
+                }}
+              >
+                <p className="">Verify</p>
+              </Button>
+            </div>
+          </Card>
+        );
+      })}
+    </ApprovalsList>
   );
 }
