43051a7b9bedd8ed366262099269160a4e2a607a
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-07 19:31:14 +0800

    Add ChangePassword

--- repositories/user/types.ts
+++ repositories/user/types.ts
@@ -8,4 +8,12 @@ export type GetUsers = (query: IQuery) => PromiseUserResponse;
 export type GetUserById = (id: string) => PromiseUserResponse;
 export type CreateUser = (request: any) => PromiseUserResponse;
 export type UpdateUser = (id: string, request: any) => PromiseUserResponse;
+
+export interface ChangePasswordArgs {
+  password: string;
+}
+export type ChangePassword = (
+  id: string,
+  request: ChangePasswordArgs
+) => PromiseUserResponse;
 export type DeleteUser = (id: string) => PromiseUserResponse;
