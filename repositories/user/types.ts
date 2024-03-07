5a0d97db9b30aad697e7f2867ebd3c4a8e828035
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-07 19:40:43 +0800

    Add verifyCustomer

--- repositories/user/types.ts
+++ repositories/user/types.ts
@@ -23,4 +23,50 @@ export interface AuthenticateArgs {
 }
 export type Authenticate = (request: AuthenticateArgs) => PromiseUserResponse;
 
+export type VerifyCustomer = (id: string) => PromiseUserResponse;
+
 export type DeleteUser = (id: string) => PromiseUserResponse;
+
+// TODO: Update User Repository with discriminators
+// import { IHttpResponse, IQuery } from "@/interfaces";
+// import {
+//   IAdminModel,
+//   ICustomerModel,
+//   IRetailerModel,
+//   IUserModel,
+// } from "@/models";
+
+// export type UserType =
+//   | IUserModel
+//   | IAdminModel
+//   | IRetailerModel
+//   | IRetailerModel
+//   | ICustomerModel;
+// export type UserResponse<T> = IHttpResponse<T>;
+// export type PromiseUserResponse<T> = Promise<UserResponse<T>>;
+
+// export type GetUsers = (query: IQuery) => PromiseUserResponse<UserType>;
+// export type GetUserById = (id: string) => PromiseUserResponse<UserType>;
+// export type CreateUser = (request: any) => PromiseUserResponse<UserType>;
+// export type UpdateUser = (
+//   id: string,
+//   request: any
+// ) => PromiseUserResponse<UserType>;
+
+// export interface ChangePasswordArgs {
+//   password: string;
+// }
+// export type ChangePassword = (
+//   id: string,
+//   request: ChangePasswordArgs
+// ) => PromiseUserResponse<UserType>;
+
+// export interface AuthenticateArgs {
+//   email: string;
+//   password: string;
+// }
+// export type Authenticate = (
+//   request: AuthenticateArgs
+// ) => PromiseUserResponse<UserType>;
+
+// export type DeleteUser = (id: string) => PromiseUserResponse<UserType>;
