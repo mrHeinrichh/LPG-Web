b6bd59d8a13594f4fad669fdbf27bf3d3b1fa0c2
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-07 19:27:26 +0800

    Add userRepository

---
+++ repositories/user/types.ts
@@ -0,0 +1,11 @@
+import { IHttpResponse, IQuery } from "@/interfaces";
+import { IUserModel } from "@/models";
+
+export type UserResponse = IHttpResponse<IUserModel>;
+export type PromiseUserResponse = Promise<UserResponse>;
+
+export type GetUsers = (query: IQuery) => PromiseUserResponse;
+export type GetUserById = (id: string) => PromiseUserResponse;
+export type CreateUser = (request: any) => PromiseUserResponse;
+export type UpdateUser = (id: string, request: any) => PromiseUserResponse;
+export type DeleteUser = (id: string) => PromiseUserResponse;
