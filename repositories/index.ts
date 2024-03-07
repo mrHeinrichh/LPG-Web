92b6a6c1a3c73d399f65e569c826dc56d3b09ab2
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-07 19:21:52 +0800

    Add itemRepository

--- repositories/index.ts
+++ repositories/index.ts
@@ -2,9 +2,13 @@ import announcementRepository from "./announcement";
 import uploadRepository from "./upload";
 import faqRepository from "./faq";
 import feedbackRepository from "./feedback";
+import itemRepository from "./item";
 export {
+  itemRepository,
   feedbackRepository,
   faqRepository,
   uploadRepository,
   announcementRepository,
 };
+
+// TODO: Add GeoApify Repository
