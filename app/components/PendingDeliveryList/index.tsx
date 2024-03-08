8dc627021e23759dc43c20fb6d2030d8b3f991b5
mrHeinrichh <johnheinrich.fabros@tup.edu.ph>
2024-03-08 16:25:28 +0800

    update app/components/PendingDeliveryList/index.tsx

--- app/components/PendingDeliveryList/index.tsx
+++ app/components/PendingDeliveryList/index.tsx
@@ -51,42 +51,73 @@ export default function PendingDeliveryList() {
         </div>
       }
     >
-      {pendings.map((e: any) => {
-        return (
-          <Card key={e._id}>
-            <div className="flex justify-between items-center w-full p-2">
-              <div className="flex flex-col">
-                <p className="text-lg font-semibold">{e.name}</p>
-                <p className="">{e.status}</p>
-                <p className="">No. of Items: {e.items.length}</p>
-                <p className="">₱ {e.total}</p>
-                {e.discountIdImage != null ? (
-                  <p className="">Discounted</p>
-                ) : (
-                  <></>
-                )}
-              </div>
+   {pendings.map((e: any) => {
+  return (
+    <Card key={e._id}>
+      <div className="flex justify-between items-center w-full p-2">
+        <div className="flex flex-col">
+          <p className="text-lg font-semibold">{e.name}</p>
+          <p className="text-lg font-semibold">{e.contactNumber}</p>
+          <p className="text-lg font-semibold">{e.deliveryLocation}</p>
+          <p className="text-lg font-semibold">{e.houseLotBlk}</p>
+          <p className="text-lg font-semibold">{e.barangay}</p>
 
-              <div className="flex items-center gap-2">
-                <Button
-                  onClick={() => {
-                    approve(e._id);
-                  }}
-                >
-                  <p className="">Approve</p>
-                </Button>
-                <Button
-                  onClick={() => {
-                    decline(e._id);
-                  }}
-                >
-                  <p className="text-white-100">Decline</p>
-                </Button>
+
+          <p className="text-lg font-semibold">{e.paymentMethod}</p>
+          
+          <p className="text-lg font-semibold">{e.deliveryDate}</p>
+
+
+
+          <p className="">{e.status}</p>
+
+          <div className="flex flex-col">
+            {e.items.map((item: any) => (
+              <div key={item._id}>
+                <p className="text-sm">{item.name}</p>
+                
+                <p className="text-xs">{item.category}</p>
               </div>
+            ))}
+          </div>
+
+          <p className="">No. of Items: {e.items.length}</p>
+          <p className="">₱ {e.total}</p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          {e.discountIdImage != null && (
+            <div className="flex flex-col items-center">
+              <p className="">Discounted</p>
+              <img src={e.discountIdImage} alt="Discount" className="w-56 h-56" />
             </div>
-          </Card>
-        );
-      })}
+          )}
+
+          <div className="flex gap-2">
+            <Button
+              onClick={() => {
+                approve(e._id);
+              }}
+            >
+              <p className="">Approve</p>
+            </Button>
+            <Button
+              onClick={() => {
+                decline(e._id);
+              }}
+            >
+              <p className="text-white-100">Decline</p>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+})}
+
+
+
+
     </ApprovalsList>
   );
 }
