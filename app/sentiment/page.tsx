f6c659708bdb02302d45075628d27c90eea72697
Anthony-CGM <anthonycharles.madrid@tup.edu.ph>
2024-03-09 03:49:34 +0800

    update

--- app/sentiment/page.tsx
+++ app/sentiment/page.tsx
@@ -3,15 +3,35 @@ import React from 'react';
 import { Sidenav } from "@/components";
 import Iframe from 'react-iframe';
 
-export default function Transactions() {
-  // Sample link to a Wikipedia page
-  const linkToDisplay = 'https://lpg-excel-webapp-streamlit.onrender.com';
+interface TransactionsProps {
+  // Add any specific props if needed
+}
+
+const containerStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+  margin: '10px', // Adjust margin as needed
+};
+
+const iframeStyle: React.CSSProperties = {
+  flex: 1,
+  border: 'none',
+  margin: '10px', // Adjust margin as needed
+  paddingLeft: '300px',
+};
+
+const Transactions: React.FC<TransactionsProps> = () => {
+  const iframeUrl = 'http://localhost:8501'; // Change to the appropriate URL
 
   return (
-    <>
+    <div style={containerStyle}>
       <Sidenav />
-      <Iframe url={linkToDisplay} width="100%" height="900px" />
-    </>
+      <div style={iframeStyle}>
+        <Iframe url={iframeUrl} width="100%" height="100%" />
+      </div>
+    </div>
   );
 }
 
+export default Transactions;
